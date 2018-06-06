1ce058c3b211bcdd91361c84f814f5aaf9109d2d
princessmittens <an747455@dal.ca>
2018-06-06 16:42:42 -0300

    added css/html for login and sign up page

--- what_course/config/routes.rb
+++ what_course/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'startup#registration'
+  root 'startup#login'
+
+  get 'startup/registration'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
